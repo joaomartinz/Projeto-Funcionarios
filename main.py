e8bbf372f86f9d6f6cdc8d57d9ecf04d159ebda3
joaomartinsz <joao.vitormg@outlook.com>
2025-09-05 18:23:10 -0300

    adicionando algumas funções

--- main.py
+++ main.py
@@ -8,11 +8,24 @@ class Funcionario:
         self.salario = salario
         self.departamento_id = departamento_id
 
+    def __str__(self):
+        return f"Funcionario(id={self.id}, nome={self.nome}, idade={self.idade}, cargo={self.cargo}, salario={self.salario}, departamento_id={self.departamento_id})"
+
 
 class Departamento:
     def __init__(self, id, nome):
         self.id = id
         self.nome = nome
+
+    def adicionar_funcionario(self, funcionario):
+        self.funcionarios.append(funcionario)
+
+    def remover_funcionario(self, funcionario):
+        self.funcionarios.remove(funcionario)
+
+    def listar_funcionarios(self):
+        return self.funcionarios
+    
 
 
 class Empresa:
@@ -20,8 +33,24 @@ class Empresa:
         self.departamentos = []
         self.funcionarios = []
    
-    def adicionar_departamento(self, departamento):
+    def cadastrar_departamento(self, departamento):
         self.departamentos.append(departamento)
 
-    def adicionar_funcionario(self, funcionario):
+    def cadastrar_funcionario(self, funcionario):
         self.funcionarios.append(funcionario)
+
+    def buscar_funcionario_por_id(self, id):
+        for funcionario in self.funcionarios:
+            if funcionario.id == id:
+                return funcionario
+        return None
+    
+    def listar_funcionarios(self):
+        return self.funcionarios
+    
+    def listar_departamentos(self):
+        return self.departamentos
+
+    def remover_funcionario(self, funcionario):
+        self.funcionarios.remove(funcionario)
+        
